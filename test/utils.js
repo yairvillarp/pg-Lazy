3c9901d4f47734c287e730049a74d87ac917a875
Uni Sayo <unibtc@devpod.io>
2019-11-18 13:31:22 +0000

    fix travis tests

--- test/utils.js
+++ test/utils.js
@@ -1,12 +1,26 @@
 // a public sample database
-module.exports = {
-  connectionString: 'postgres://tojrrrso:demc16cCK1w9MOJavZY6GEoDe-kj9y36@raja.db.elephantsql.com:5432/tojrrrso',
-  connectionObject: {
-    user: 'tojrrrso',
-    host: 'raja.db.elephantsql.com',
-    database: 'tojrrrso',
-    password: 'demc16cCK1w9MOJavZY6GEoDe-kj9y36',
-    port: 5432
-  },
-  queryError: /must build query with sql or _raw/
-};
+if(process.NODE_ENV == 'travis') {
+  module.exports = {
+    connectionString: 'postgres://lazy_test_user:lazy_test_pw@localhost:5432/lazy_test',
+    connectionObject: {
+      user: 'lazy_test_user',
+      host: 'localhost',
+      database: 'lazy_test',
+      password: 'lazy_test_pw',
+      port: 5432
+    },
+    queryError: /must build query with sql or _raw/
+  }
+}else {
+  module.exports = {
+    connectionString: 'postgres://tojrrrso:demc16cCK1w9MOJavZY6GEoDe-kj9y36@raja.db.elephantsql.com:5432/tojrrrso',
+    connectionObject: {
+      user: 'tojrrrso',
+      host: 'raja.db.elephantsql.com',
+      database: 'tojrrrso',
+      password: 'demc16cCK1w9MOJavZY6GEoDe-kj9y36',
+      port: 5432
+    },
+    queryError: /must build query with sql or _raw/
+  };
+}
